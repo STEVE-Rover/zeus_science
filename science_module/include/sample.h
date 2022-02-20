50ad37ca02fd5a03af630d21bc770536bf02e57c
Vincent Pelletier <42898536+Plopao@users.noreply.github.com>
2022-02-19 21:02:20 -0500

    added sample_information to class

--- science_module/include/sample.h
+++ science_module/include/sample.h
@@ -3,16 +3,27 @@
 class Sample
 {
 private:
-    int life = -1; // -1 stands for "nothing was done yet"
+    int life; // -1 stands for "nothing was done yet on the sample"
+    float ph; //from 0 to 14
+    float humidity; //in ppm
+
 public:
     Sample();
     ~Sample();
     void set_life_info(int presence);
     int get_life_info();
+    void set_humidity_info(float humidity_value);
+    int get_humidity_info();
+    void set_ph_info(float ph_value);
+    int get_ph_info();
 };
 
 Sample::Sample()
-{}
+{
+    life = -1;
+    ph = -1.0;
+    humidity = -1.0;
+}
 
 Sample::~Sample()
 {}
@@ -23,4 +34,20 @@ void Sample::set_life_info(int presence){
 
 int Sample::get_life_info(){
     return life;
+}
+
+void Sample::set_humidity_info(float humidity_value){
+    humidity = humidity_value;
+}
+
+int Sample::get_humidity_info(){
+    return humidity;
+}
+
+void Sample::set_ph_info(float ph_value){
+    ph = ph_value;
+}
+
+int Sample::get_ph_info(){
+    return ph;
 }
